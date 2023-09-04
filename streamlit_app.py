8d189ec4ce83bcf00d0600ae41cf103dbd80b061
pooja-app18 <143996524+pooja-app18@users.noreply.github.com>
2023-09-04 23:58:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 return fruityvice_normalized
@@ -38,7 +38,7 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-
+#def get_fruit_load_list()
 
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
